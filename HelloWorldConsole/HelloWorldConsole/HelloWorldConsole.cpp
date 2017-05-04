b9f22fd47462074390a35d5d846687df8c3a851e
wallace123 <wallace123@github.com>
2017-05-04 07:32:20 -0400

    Checking in Hello World Console application

---
+++ HelloWorldConsole/HelloWorldConsole/HelloWorldConsole.cpp
@@ -0,0 +1,13 @@
+// HelloWorldConsole.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Hello World\n");
+    return 0;
+}
+
